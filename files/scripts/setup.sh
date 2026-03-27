d48ba54a70fd60ee382f1c64780362647aaad246
SJ7449 <podeswik.sp@gmail.com>
2026-03-27 18:27:08 -0400

    Update setup.sh

--- files/scripts/setup.sh
+++ files/scripts/setup.sh
@@ -3,4 +3,5 @@ set -e
 plymouth-set-default-theme bazzite -R
 chmod +x /usr/bin/clean-system
 chmod +x /usr/lib/systemd/system/battery-limit.service
-systemctl enable battery-limit.service
+ln -s /usr/lib/systemd/system/battery-limit.service \
+      /etc/systemd/system/multi-user.target.wants/battery-limit.service
